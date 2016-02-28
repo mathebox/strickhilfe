387a3e736e037eda24b1da86f7e8442a7a282d2c
Max Bothe <mathebox@gmail.com>
2016-02-28 17:37:29 +0100

    Rework letter A.
    - Add spacer for letter A.

--- static/js/main.js
+++ static/js/main.js
@@ -74,55 +74,69 @@ function drawA(width, height, a, b) {
     var ratio = realWidth/Math.max(width, height);
     var alpha = Math.atan(2*height/width);
     // big shape
-    var x1_a = realWidth/2 - (width/2*ratio);
-    var y1_a = realHeight/2 + (height/2*ratio);
-    var x1_b = x1_a + a*ratio;
-    var y1_b = y1_a;
-    var x1_c = x1_b + b/Math.tan(alpha)*ratio;
-    var y1_c = y1_b - b*ratio;
-    var x1_d = realWidth/2 + (width/2 - a - b/Math.tan(alpha))*ratio;
-    var y1_d = y1_c;
-    var x1_e = x1_d + b/Math.tan(alpha)*ratio;
-    var y1_e = y1_b;
-
-    var d = `M ${realWidth/2-(width/2*ratio)},${realHeight/2+(height/2*ratio)}`;
-    d += ` L ${realWidth/2-(width/2*ratio)+a*ratio},${realHeight/2+(height/2*ratio)}`;
-    d += ` L ${realWidth/2-(width/2*ratio)+a*ratio+b*ratio*Math.cos(alpha)},${realHeight/2+(height/2*ratio)-b*ratio*Math.sin(alpha)}`;
-    d += ` L ${realWidth/2+(width/2*ratio)-a*ratio-b*ratio*Math.cos(alpha)},${realHeight/2+(height/2*ratio)-b*ratio*Math.sin(alpha)}`;
-
-    d += ` L ${realWidth/2+(width/2*ratio)-a*ratio},${realHeight/2+(height/2*ratio)}`;
-    d += ` L ${realWidth/2+(width/2*ratio)},${realHeight/2+(height/2*ratio)}`;
-    d += ` L ${realWidth/2},${realHeight/2-(height/2*ratio)}`;
-    d += `z`;
+    var widthOffset1 = (width/2)*ratio;
+    var widthOffset2 = (width/2-a)*ratio;
+    var widthOffset3 = (width/2-a-b/Math.tan(alpha))*ratio;
+    var heightOffset1 = (height/2)*ratio;
+    var heightOffset2 = (height/2-b)*ratio;
+    var dataPointsBig = new Array();
+    dataPointsBig.push({'x': realWidth/2 - widthOffset1, 'y': realHeight/2 + heightOffset1});  // a
+    dataPointsBig.push({'x': realWidth/2 - widthOffset2, 'y': realHeight/2 + heightOffset1});  // b
+    dataPointsBig.push({'x': realWidth/2 - widthOffset3, 'y': realHeight/2 + heightOffset2}); // c
+    dataPointsBig.push({'x': realWidth/2 + widthOffset3, 'y': realHeight/2 + heightOffset2}); // d
+    dataPointsBig.push({'x': realWidth/2 + widthOffset2, 'y': realHeight/2 + heightOffset1}); // e
+    dataPointsBig.push({'x': realWidth/2 + widthOffset1, 'y': realHeight/2 + heightOffset1}); // f
+    dataPointsBig.push({'x': realWidth/2, 'y': realHeight/2 - heightOffset1}); // g
 
     // small hole
-    d += `M ${realWidth/2},${realHeight/2-(height/2*ratio)+a*ratio*Math.tan(alpha)}`
-    d +=` L ${realWidth/2+(width/2*ratio)-a*ratio-((b*Math.sin(alpha)+a*Math.sin(alpha))/Math.tan(alpha)*ratio)},${realHeight/2+(height/2*ratio)-b*ratio*Math.sin(alpha)-a*ratio*Math.sin(alpha)}`;
-    d += ` L ${realWidth/2-(width/2*ratio)+a*ratio+((b*Math.sin(alpha)+a*Math.sin(alpha))/Math.tan(alpha)*ratio)},${realHeight/2+(height/2*ratio)-b*ratio*Math.sin(alpha)-a*ratio*Math.sin(alpha)}`;
-    d += `z`;
+    var dataPointsSmall = new Array();
+    var widthOffset4 = widthOffset3 - a*Math.sin(alpha)/Math.tan(alpha)*ratio;
+    var heightOffset3 = height/2 - a/Math.tan(alpha)*ratio;
+    var heightOffset4 = heightOffset2 - a*Math.sin(alpha)*ratio;
+    dataPointsSmall.push({'x': realWidth/2, 'y': realHeight/2 + heightOffset3})
+    dataPointsSmall.push({'x': realWidth/2 + widthOffset4, 'y': realHeight/2 + heightOffset4})
+    dataPointsSmall.push({'x': realWidth/2 - widthOffset4, 'y': realHeight/2 + heightOffset4})
+
+    var d = 'M' + $.map(dataPointsBig, function(el) { return el.x + ' ' + el.y}).join('L') + 'z';
+    d += 'M' + $.map(dataPointsSmall, function(el) { return el.x + ' ' + el.y}).join('L') + 'z';
+
     p = paper.path(d);
     p.node.id = 'raphael-letter';
     p.attr({
         'stroke': 0
     });
 
-    var t = paper.text(realWidth/2-(width/2*ratio)+a*ratio/2, realHeight/2+(height/2*ratio)-30, "a").attr({
-        'font-size': 50
-    });
+    // a
+    p = drawSpacer(
+        realWidth/2 - (width/2-b/2/Math.tan(alpha))*ratio,
+        realHeight/2 + (height/2-b/2)*ratio,
+        realWidth/2 - (width/2-b/2/Math.tan(alpha)-a)*ratio,
+        realHeight/2 + (height/2-b/2)*ratio);
+    p.node.id = 'raphael-spacer-light';
+    var t = drawSpacerText('a',
+        realWidth/2 - (width/2-b/2/Math.tan(alpha)-a/2)*ratio,
+        realHeight/2 + (height/2-b/2)*ratio - 30);
     t.node.id = 'raphael-text-light';
-    t = paper.text(realWidth/2+(width/2*ratio)-a*ratio-b*ratio*Math.cos(alpha)/2+30, realHeight/2+(height/2*ratio)-b*ratio*Math.sin(alpha)/2, "b").attr({
-        'font-size': 50
-    });
-    t.node.id = 'raphael-text-light';
+
+    // b
+    p = drawSpacer(
+        realWidth/2,
+        realHeight/2 + (height/2-b)*ratio,
+        realWidth/2,
+        realHeight/2 + (height/2)*ratio);
+    p.node.id = 'raphael-spacer-dark';
+    t = drawSpacerText('b', realWidth/2 + 30, realHeight/2 + (height/2-b/2)*ratio);
+    t.node.id = 'raphael-text-dark';
 }
 
 function calculatePerimeterA(width, height, a, b) {
     var alpha = Math.atan(2*height/width);
+    var b1 = b/Math.sin(alpha)
     var c = Math.sqrt(height*height + width*width);
     var d = width-2*(a+Math.cos(alpha)*b)
-    var f = width-2*(a+(b*Math.sin(alpha)+a*Math.sin(alpha))/Math.tan(alpha))
-    var e = Math.sqrt(Math.pow(height-b*Math.sin(alpha)-a*Math.sin(alpha)-a*Math.tan(alpha), 2) + Math.pow(f/2, 2));
-    var perimeter1 = (2*(a+b+c)+d).toFixed(1);
+    var f = width-2*(a+(b1*Math.sin(alpha)+a*Math.sin(alpha))/Math.tan(alpha))
+    var e = Math.sqrt(Math.pow(height-b1*Math.sin(alpha)-a*Math.sin(alpha)-a*Math.tan(alpha), 2) + Math.pow(f/2, 2));
+    var perimeter1 = (2*(a+b1+c)+d).toFixed(1);
     var perimeter2 = (2*e+f).toFixed(1);
     $('#perimeter').html('Umfang 1: ' + perimeter1 + 'cm<br/>Umfang 2: ' + perimeter2 + 'cm');
 }
